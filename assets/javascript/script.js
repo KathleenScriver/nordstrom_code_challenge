781e21be1bbc866a9b142b3f64a6f0fe23fa0612
Kathleen Yruegas <kathleenecn@gmail.com>
2018-12-27 10:44:10 -0700

    Adds POST fetch for new todo item.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,8 +10,9 @@ const getAllTodos = async () => {
 }
 
 const displayAllTodos = (allTodos) => {
+  $('.todo-section').html(' ');
   allTodos.forEach(todo => {
-    $('#todo-list').append(`
+    $('.todo-section').append(`
       <tr>
         <td>${todo.todoDescription}</td>
         <td>${todo.tag}</td>
@@ -20,8 +21,27 @@ const displayAllTodos = (allTodos) => {
   });
 };
 
+const addNewTodo = async () => {
+  const newTodoDesc = $('#description').val();
+  const newTodoTag = $('#tag').val();
+
+  try {
+    const response = await fetch('https://cq31v4skne.execute-api.us-east-2.amazonaws.com/beta/todos', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        todoDescription: newTodoDesc,
+        tag: newTodoTag
+      })
+    });
+    getAllTodos();
+  } catch (error) {
+    console.log(error.message)
+  }
+}
 
 
+$('#submit-todo').on('click', addNewTodo)
 
 // Get all ToDos on page load
 
